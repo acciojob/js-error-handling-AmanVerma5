680b5f6b3dae62e724fd7b800c2263f721e87483
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-05 15:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your code here
+class OutOfRangeError extends Error{
+	constructor(){
+		super();
+		this.name=this.constructor.name;
+		this.message="Expression should only consist of integers and +-/* characters and not < arg >";
+	}
+}
+class InvalidExprError extends Error{
+	constructor(){
+		super();
+		this.name=this.constructor.name;
+		this.message="Expression should not have an invalid combination of expression";
+	}
+}
+
+function evalString(expression){
+	try{
+		if(/[\d+-/*\s]/.test(expression)==false) throw new OutOfRangeError();
+
+		if(/(\+\+|\-\-|\*\*|\/\/|\+\-|\+\*|\/\*|\/\+|\-\+|\-\*|\/\-|\*\+|\*\-)/.test(expression)) throw new InvalidExprError();
+
+		 if (/^[\/*+].*/.test(expression)) {
+		      throw new SyntaxError();
+		    }
+
+	    if (/.*[\/*+-]$/.test(expression)) {
+	      throw new SyntaxError();
+	    }
+		 return "Expression is valid and can be evaluated.";
+	}catch(error){
+		console.error(`${error.name}: ${erroe.message}`);
+	}
+}
